680606abdfceeb2abb94b4f4abfb1d6f087b5f76
roshan <roshanprusty100@gmail.com>
2023-06-13 01:49:06 +0530

    3

--- intro.TS.js
+++ intro.TS.js
@@ -1,3 +1,3 @@
-let user ={name:"Roshan", age:21};
+var user = { name: "Roshan", age: 21 };
 // let email= user.email; // here you can see an error where as in js you will be not seeing error even though data is missing. Hope now you are Why Type script is require and how it can play vital role.
 console.log(user.name); //Roshan
