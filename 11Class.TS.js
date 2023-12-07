ec912e0d1e5534d492697a56f84413bedb2b4039
roshan <roshanprusty100@gmail.com>
2023-12-07 16:13:54 +0530

    class, concepts, API

---
+++ 11Class.TS.js
@@ -0,0 +1,8 @@
+var User = /** @class */ (function () {
+    function User(email, name) {
+        this.email = email;
+        this.name = name;
+    }
+    return User;
+}());
+var rnp = new User("roshan@yahoo.com", "roshan");
